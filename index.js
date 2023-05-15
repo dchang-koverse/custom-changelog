d6080aa4d0f5b3f62aa78df57f83c7d1c4b73f92
Dale Chang <dalechang@koverse.com>
2023-05-15 10:34:17 -0700

    chore(script): update to get messages and tags

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ const octokit = new Octokit({
     // auth: process.env.TOKEN
 });
 
-try {
-    const result = await octokit.request("GET /repos/{owner}/{repo}/commits", {
-        owner: "dchang-koverse",
-        repo: "custom-changelog",
-    });
-    console.log('result', result.data)
-
-    // const titleAndAuthor = result.data.map(issue => {
-    //     title: issue.title,
-    //     authorID: issue.user.id
-    // })
-
-    // console.log(titleAndAuthor)
-
-} catch (error) {
-    console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+const getCommitMessages = async () => {
+    try {
+        console.log('GETting commit messages')
+        const result = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+            owner: "dchang-koverse",
+            repo: "custom-changelog",
+        });
+    
+        const messages = result.data.map(datum => datum.commit.message)
+        console.log('commit messages', messages)
+    
+        return messages
+    } catch (error) {
+        console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+    }
 }
+
+const getTags = async () => {
+    try {
+        console.log('GETting tags')
+        const result = await octokit.request("GET /repos/{owner}/{repo}/tags", {
+            owner: "dchang-koverse",
+            repo: "custom-changelog",
+        });
+    
+        console.log('tags', result.data)
+    } catch (error) {
+        console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+    }
+}
+
+getCommitMessages()
+getTags()
